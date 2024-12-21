9c46e906e7db6105855db966761b2de62944b990
wtei7 <m45a1@dgsw.hs.kr>
2024-12-21 20:45:01 +0900

    fix :: syntax 오류 및 경고 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "src/pages/"
+import Main from "src/pages/main";
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" component={<Main/>} />
+                <Route path="/" element={<Main />} />
             </Routes>
         </BrowserRouter>
     );
