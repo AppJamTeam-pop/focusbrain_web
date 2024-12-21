d21fef6b084db20fec1d99b5e602ea93d61ea3f4
wtei7 <m45a1@dgsw.hs.kr>
2024-12-21 19:48:19 +0900

    fix :: 경로 관련 오류 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Main from "src/pages/main"
+import Main from "src/pages/"
 const Router = () => {
     return (
         <BrowserRouter>
