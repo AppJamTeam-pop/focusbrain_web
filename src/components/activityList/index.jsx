28fe0a80c1b38a67a85755a652278a30419692e2
wtei7 <m45a1@dgsw.hs.kr>
2024-12-22 10:57:54 +0900

    feat

--- src/components/activityList/index.jsx
+++ src/components/activityList/index.jsx
@@ -3,13 +3,13 @@ import * as S from './style';
 import Activity from "src/components/activity";
 
 const ActivityList = () => {
-    const [activity, setActivity] = React.useState([{title:'스트레칭하기',href:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},{title:'스트레칭하기',href:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},{title:'스트레칭하기',href:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},{title:'스트레칭하기',href:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},{title:'스트레칭하기',href:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},{title:'스트레칭하기',href:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},{title:'스트레칭하기',href:'https://www.youtube.com/watch?v=dQw4w9WgXcQ'},]);
+    const [activity, setActivity] = React.useState([{title:'스트레칭하기',href:'https://www.youtube.com/results?search_query=스트레칭하기'},{title:'명상하기',href:'https://www.youtube.com/results?search_query=명상하기'},{title:'DIY 만들기',href:'https://www.youtube.com/results?search_query=DIY 만들기'},{title:'프라모델 만들기',href:'https://www.youtube.com/results?search_query=프라모델 만들기'},{title:'도움이 되는 책 읽기',href:'https://www.youtube.com/results?search_query=도움이 되는 책 읽기'},]);
     return (
         <S.Wrapper>
             <S.Title>AI 추천 맞춤 활동</S.Title>
             <S.ListContainer>
                 {activity.map((item, i) => (
-                    <Activity title={item.title} href={item.href}></Activity>
+                    <Activity key={i} title={item.title} href={item.href}></Activity>
                 ))}
             </S.ListContainer>
         </S.Wrapper>
